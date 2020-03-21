43d0b90f4f778ab57ff0c248ed1f5a4722fd65f3
Thomas <thomas@thomass-mbp.lan>
2020-03-21 13:59:14 -0400

    added retry option

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var gameStarted = false;
+var count = 0;
 var subreddits = ["TheOnion", "nottheonion"];
 var newHeadline = "";
 var retrievedSubreddit = "";
@@ -6,8 +6,10 @@ var tries = 3;
 var score = 0;
 
 function makeApiCall(url, randNum, randNum2) {
+   $("#article-headline").text(" ");
    console.log(url);
-
+   count++;
+   console.log(count);
    $.ajax(url,   // request url
    {
       success: function (data, status, xhr) {    // success callback function
@@ -33,42 +35,83 @@ $("#start-button").on("click", function () {
    $("#tries").removeClass("hidden");
    $("#score").removeClass("hidden");
 
-   $("#score").text("Score: 0");
+   $("#score").text("Score: 0/10");
    $("#tries").text("Tries: 3");
 });
 
 
 $("#correct-button").on("click", function () {
+   if(count === 10){
+      console.log("End quiz");
+   }
    if(retrievedSubreddit === "TheOnion"){
       console.log("Correct");
       score++;
-      $("#score").text("Score: " + score);
-      var randNum = Math.random() < 0.5 ? 0 : 1;;
-      var randNum2 = Math.floor(Math.random() * (24 - 0 + 1)) + 0;
-   
-      var api_url = "https://www.reddit.com/r/" + subreddits[randNum] + "/new.json?sort=popular";
-      makeApiCall(api_url, randNum, randNum2);
+      $("#score").text("Score: " + score + "/10");
+      if(score === 10){
+         gameWon();
+      }else{
+         var randNum = Math.random() < 0.5 ? 0 : 1;;
+         var randNum2 = Math.floor(Math.random() * (24 - 0 + 1)) + 0;
+      
+         var api_url = "https://www.reddit.com/r/" + subreddits[randNum] + "/new.json?sort=popular";
+         makeApiCall(api_url, randNum, randNum2);
+      }
    }else{
       tries--;
+      if(tries === 0){
+         ranOutOfTries();
+      }
       console.log("Wrong");
       $("#tries").text("Tries: " + tries);
    }
 });
 
 $("#wrong-button").on("click", function () {
+   if(count === 10){
+      console.log("End quiz");
+   }
    if(retrievedSubreddit === "nottheonion"){
       console.log("Correct");
       score++;
-      $("#score").text("Score: " + score);
-
-      var randNum = Math.random() < 0.5 ? 0 : 1;;
-      var randNum2 = Math.floor(Math.random() * (24 - 0 + 1)) + 0;
-   
-      var api_url = "https://www.reddit.com/r/" + subreddits[randNum] + "/new.json?sort=popular";
-      makeApiCall(api_url, randNum, randNum2);
+      $("#score").text("Score: " + score + "/10");
+      if(score === 10){
+         gameWon();
+      }else{
+         var randNum = Math.random() < 0.5 ? 0 : 1;;
+         var randNum2 = Math.floor(Math.random() * (24 - 0 + 1)) + 0;
+      
+         var api_url = "https://www.reddit.com/r/" + subreddits[randNum] + "/new.json?sort=popular";
+         makeApiCall(api_url, randNum, randNum2);
+      }
    }else{
       tries--;
+      if(tries === 0){
+         ranOutOfTries();
+      }
       console.log("Wrong");
       $("#tries").text("Tries: " + tries);   
    }
 });
+
+function ranOutOfTries() {
+      // Get the modal
+   var modal = document.getElementById("myModal");
+   modal.style.display = "block";
+
+   $("#try-again").on("click", function () {
+      modal.style.display = "none";
+      tries = 3;
+      score = 0;
+      count = 0;
+      $("#score").text("Score: 0/10");
+      $("#tries").text("Tries: 3");
+   });
+}
+
+function gameWon() {
+   $("#article-headline").text("Congrats you passed the quiz!");
+
+   document.getElementById("correct-button").style.visibility = "hidden";
+   document.getElementById("wrong-button").style.visibility = "hidden";
+};
